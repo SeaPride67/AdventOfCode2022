0d339f15efcb74d89c6dfc8524a7ed32cfdfa2b2
Corey Prideaux <cpride67@protonmail.com>
2022-12-18 11:43:48 -0600

    Day1 part2 complete

--- Day1/CalorieCounting/Program.cs
+++ Day1/CalorieCounting/Program.cs
@@ -11,10 +11,24 @@ namespace CalorieCounting
     class Program
     {
 
+        private class SortIntDescending : IComparer<int>
+        {
+            int IComparer<int>.Compare(int a, int b) //implement Compare
+            {
+                if (a > b)
+                    return -1; //normally greater than = 1
+                if (a < b)
+                    return 1; // normally smaller than = -1
+                else
+                    return 0; // equal
+            }
+        }
+
         static int maxCalories = 0;
         static int minCalories = 9999999;
         static int curValue = 0;
         static int curCalories = 0;
+        static List<int> listCalories;
 
         static void Main(string[] args)
         {
@@ -28,6 +42,8 @@ namespace CalorieCounting
             StreamReader sr = new StreamReader("input.txt");
             string dataIn;
 
+            listCalories = new List<int>();
+
             while (sr.Peek() >= 0)
             {
 
@@ -35,6 +51,7 @@ namespace CalorieCounting
                 if (dataIn.Trim().Length == 0)
                 {
                     System.Console.WriteLine("Empty");
+                    listCalories.Add(curCalories);
                     if (curCalories <= minCalories) minCalories = curCalories;
                     if (curCalories >= maxCalories) maxCalories = curCalories;
                     curCalories = 0;
@@ -60,6 +77,11 @@ namespace CalorieCounting
             System.Console.WriteLine("minCalories=" + minCalories.ToString());
             System.Console.WriteLine("maxCalories=" + maxCalories.ToString());
 
+            listCalories.Sort(new SortIntDescending());
+            System.Console.WriteLine("listCalories[0]=" + listCalories[0].ToString());
+            System.Console.WriteLine("listCalories[1]=" + listCalories[1].ToString());
+            System.Console.WriteLine("listCalories[2]=" + listCalories[2].ToString());
+            System.Console.WriteLine("sum of top three = " + (listCalories[0]+ listCalories[1]+ listCalories[2]).ToString());
             sr.Close();
 
             return (true);
