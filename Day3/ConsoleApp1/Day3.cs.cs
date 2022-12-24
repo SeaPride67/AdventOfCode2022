57b7af3b4aaafc979c2c469e2a427e6e53f15b94
Corey Prideaux <cpride67@protonmail.com>
2022-12-24 02:24:22 -0600

    Day3, part 2 complete

--- Day3/ConsoleApp1/Day3.cs.cs
+++ Day3/ConsoleApp1/Day3.cs.cs
@@ -13,28 +13,21 @@ namespace ConsoleApp1
         {
 
             StreamReader sr = new StreamReader("C:\\Users\\CoreyPrideaux\\source\\repos\\AdventOfCode2022\\input_day3.txt");
-            string dataIn;
             string dataIn1;
             string dataIn2;
+            string dataIn3;
             int i = 0;
             int j = 0;
+            int k = 0;
             int sumPriority = 0;
             int curPriority = 0;
 
             while (sr.Peek() >= 0)
             {
 
-                dataIn = sr.ReadLine();
-
-                if ((dataIn.Length % 2) != 0)
-                {
-                    System.Console.WriteLine("Invalid length for dataIn, " + dataIn.Length);
-                    System.Console.ReadKey();
-                    break;
-                } // if
-
-                dataIn1 = dataIn.Substring(0, dataIn.Length / 2);
-                dataIn2 = dataIn.Substring(dataIn.Length / 2, dataIn.Length - dataIn.Length / 2);
+                dataIn1 = sr.ReadLine();
+                dataIn2 = sr.ReadLine();
+                dataIn3 = sr.ReadLine();
 
                 for (i = 0; i < dataIn1.Length; i++)
                 {
@@ -42,23 +35,30 @@ namespace ConsoleApp1
                     {
                         if (dataIn1[i] == dataIn2[j])
                         {
-                            if (dataIn1[i] >= 'a' && dataIn1[i] <= 'z')
+                            for (k = 0; k < dataIn3.Length; k++)
                             {
-                                curPriority = (dataIn1[i] - 'a') + 1;
-                                sumPriority += curPriority;
-                            } // if
-                            else
-                            {
-                                curPriority = (dataIn1[i] - 'A') + 27;
-                                sumPriority += curPriority;
-                            } // else
-                            System.Console.WriteLine("dataIn1=" + dataIn1);
-                            System.Console.WriteLine("dataIn2=" + dataIn2);
-                            System.Console.WriteLine("dataIn1[i]=" + dataIn1[i] + "\ndataIn2[j]=" + dataIn2[j]);
-                            System.Console.WriteLine("curPriority=" + curPriority.ToString() + " sumPriority=" + sumPriority.ToString() + "\n");
-                            j = dataIn2.Length;
-                            i = dataIn1.Length;
-                        }
+                                if (dataIn2[j] == dataIn3[k])
+                                {
+                                    if (dataIn1[i] >= 'a' && dataIn1[i] <= 'z')
+                                    {
+                                        curPriority = (dataIn1[i] - 'a') + 1;
+                                        sumPriority += curPriority;
+                                    } // if
+                                    else
+                                    {
+                                        curPriority = (dataIn1[i] - 'A') + 27;
+                                        sumPriority += curPriority;
+                                    } // else
+                                    System.Console.WriteLine("dataIn1=" + dataIn1);
+                                    System.Console.WriteLine("dataIn2=" + dataIn2);
+                                    System.Console.WriteLine("dataIn1[i]=" + dataIn1[i] + "\ndataIn2[j]=" + dataIn2[j]);
+                                    System.Console.WriteLine("curPriority=" + curPriority.ToString() + " sumPriority=" + sumPriority.ToString() + "\n");
+                                    i = dataIn1.Length;
+                                    j = dataIn2.Length;
+                                    k = dataIn3.Length;
+                                } // if
+                            } // for
+                        } // if
                     } // for
                 } // for
 
